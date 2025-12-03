b78334ea9b68628c0a63441e95daad8973f8c4b4
Matheus Alexandre <130194413+eumatheusths@users.noreply.github.com>
2025-12-03 10:31:09 -0300

    Forçando atualização do config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node'; // Se estiver usando adaptador node, senão remova
+import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
-      // Força a geração do CSS base
       applyBaseStyles: true,
     }),
   ],
-  output: 'server', // Importante para SSR (Banco de dados)
-  adapter: node({
-    mode: 'standalone',
-  }),
+  output: 'server',
+  adapter: vercel(),
 });
