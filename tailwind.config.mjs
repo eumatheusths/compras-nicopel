b82e2ede881f854f7d9d64a40636effc824b2e5c
Matheus Alexandre <130194413+eumatheusths@users.noreply.github.com>
2025-12-03 11:32:05 -0300

    add

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // A linha abaixo diz pro Tailwind olhar TODOS os arquivos na pasta src
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
